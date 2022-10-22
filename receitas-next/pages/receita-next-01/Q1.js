20a1b81f1058df36d45247e865768ca2e457d3d5
rusdrael <85977202+rusdrael@users.noreply.github.com>
2022-10-22 00:01:07 -0300

    Correções

--- receitas-next/pages/receita-next-01/Q1.js
+++
@@ -1,9 +0,0 @@
-export default function Q1(){
-    return (
-        <div>
-            <h2>
-                Morra Santana!
-            </h2>
-        </div>
-    )
-}
--- receitas-next/pages/receita-next-01/Q2.js
+++
@@ -1,9 +0,0 @@
-export function Q2(){
-    return (
-        <div>
-            <h2>
-                Viva ou morra Santana!?
-            </h2>
-        </div>
-    )
-}
